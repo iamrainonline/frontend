624494c6676334248718d2ba3bd0410e0d097bae
Cristian <cristian.candidatu@itom.ro>
2023-07-23 00:43:42 +0300

    asd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
    app.use(
+      "/api", // Your API endpoint prefix
       createProxyMiddleware({
-         target: "https://dull-rose-camel-garb.cyclic.app/",
-         changeOrigin: true,
+         target: "https://dull-rose-camel-garb.cyclic.app/api",
       })
    );
 };
