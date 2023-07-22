4f28a360a842c3cf8f86ef7e2aac499a1614e118
Cristian <cristian.candidatu@itom.ro>
2023-07-22 14:50:26 +0300

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,9 @@ const Home = () => {
    useEffect(() => {
       const fetchData = async () => {
          try {
-            const res = await axios.get(`/api/posts${cat}`);
+            const res = await axios.get(
+               `https://dull-rose-camel-garb.cyclic.app/api/posts${cat}`
+            );
             setPosts(res.data.reverse());
          } catch (err) {
             console.log(err);
